7145127809c48f5d6008ee878a6c0caef54652d0
LEmax <lemaxe@gmail.com>
2015-05-03 17:49:12 +0300

    main page initial

---
+++ include/compiled/main_box_small.php
@@ -0,0 +1,26 @@
+<div class="index_small">
+    <div class="small_top">
+        	<a href="/team.php?id=<?php echo $team['id']; ?>"><img src="<?php if(1==preg_match('/http:/',$team['image']))echo $team['image']; else echo '/static/'.$team["image"]; ?>" alt="<?=$team['title']; ?>" width="380" border="0" height="213"/></a>
+    </div>
+    
+    
+    
+    
+    
+    
+    <div class="small_bottom">
+        	<div class="small_bottom_txt">
+            <a href="/team.php?id=<?php echo $team['id']; ?>"><?=$team['title'];?></a>
+            </div>
+            <div class="small_bottom_time" diff="<?php echo $diff_time; ?>000">
+             <?php //countdown($team['end_time']); ?>
+                    <span id="counter"><span class="date" rel="<?php echo ($team['end_time']); ?>"><?php countdown($team['end_time']); ?></span></span>
+            </div>
+            <div class="small_bottom_kupon">
+            <?php echo $all_days[$team['id']] ? $all_days[$team['id']]['q'] : 0; ?>
+            </div>
+            
+            <a href="<?php echo '/team.php?id='.$team['id']; ?>" class="knopka_ok"><?=(moneyit($team['team_comission'])!=0?'Купить':'Получить')?></a>
+     </div>
+  
+</div>
