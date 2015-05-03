ea992a7bd9f262ea707f59285b387dcf586cd678
LEmax <lemaxe@gmail.com>
2015-05-03 17:50:18 +0300

    main page with correct URL

--- include/compiled/main_box_small.php
+++ include/compiled/main_box_small.php
@@ -1,16 +1,16 @@
 <div class="index_small">
     <div class="small_top">
-        	<a href="/team.php?id=<?php echo $team['id']; ?>"><img src="<?php if(1==preg_match('/http:/',$team['image']))echo $team['image']; else echo '/static/'.$team["image"]; ?>" alt="<?=$team['title']; ?>" width="380" border="0" height="213"/></a>
+        	<a href="/<?php echo $team['id'].'_'.$team['a'.'li'.'as']; ?>"><img src="<?php if(1==preg_match('/http:/',$team['image']))echo $team['image']; else echo '/static/'.$team["image"]; ?>" alt="<?=$team['title']; ?>" width="380" border="0" height="213"/></a>
     </div>
     
     
-    
+    <!-- <a href="/team.php?id=<?php echo $team['id']; ?>"> -->
     
     
     
     <div class="small_bottom">
         	<div class="small_bottom_txt">
-            <a href="/team.php?id=<?php echo $team['id']; ?>"><?=$team['title'];?></a>
+            <a href="/<?php echo $team['id'].'_'.$team['a'.'li'.'as']; ?>"><?=$team['title'];?></a>
             </div>
             <div class="small_bottom_time" diff="<?php echo $diff_time; ?>000">
              <?php //countdown($team['end_time']); ?>
@@ -20,7 +20,7 @@
             <?php echo $all_days[$team['id']] ? $all_days[$team['id']]['q'] : 0; ?>
             </div>
             
-            <a href="<?php echo '/team.php?id='.$team['id']; ?>" class="knopka_ok"><?=(moneyit($team['team_comission'])!=0?'Купить':'Получить')?></a>
+            <a href="/<?php echo $team['id'].'_'.$team['a'.'li'.'as']; ?>" class="knopka_ok"><?=(moneyit($team['team_comission'])!=0?'Купить':'Получить')?></a>
      </div>
   
 </div>
