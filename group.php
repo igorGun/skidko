148eeba25c70d62eddb1af1be52ee7376362bbb3
igunyaga <igunyaga@ubuntu12.vps>
2015-05-07 13:45:37 +0300

    2nd commit group

--- group.php
+++ group.php
@@ -5,33 +5,6 @@ $idg = abs(intval($_GET['idg']));
 $idt = abs(intval($_GET['idt']));
 
 
- 
-/*$host = "localhost";
-$user = "skidkoman_user";
-$pass = "wG8Z8NqO";
-$bd_name='skidkoman_db';
-   
-    $link = mysqli_connect($host,$user,$pass,$bd_name);
-    $res = mysqli_query($link, "SELECT id,ename FROM `category`WHERE ename='$alias'" );
-   // $res =DB::GetQueryResult('SELECT id,ename FROM `category`WHERE ename="$alias"');
-   // $res = DB::Query('SELECT id,ename FROM `category`WHERE ename="$alias" ');
-
-while($row = mysqli_fetch_assoc($res)) {
-    print_r($row);
-
-    if ($alias==$row['ename']){
-        $idg=$row['id'];
-    }
-    print_r($idg);
-}*/
- $res =DB::Query("SELECT `id` FROM `category` WHERE `ename`='{$_GET['alias']}'");
-while($row = mysqli_fetch_assoc($res)) {
-    print_r($row);
-
-    // if ($alias==$row['ename']){
-        $idg=$row['id'];
-    }
-
 
 ($currefer = strval($_GET['refer'])) || ($currefer = strval($_GET['r']));
 if ($idg!='none') {
