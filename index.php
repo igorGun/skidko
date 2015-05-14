6e65693638cbfd333ce3254aec28595325703919
igunyaga <igunyaga@ubuntu12.vps>
2015-05-14 16:03:18 +0300

    2nd commit index

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 require_once(dirname(__FILE__) . '/app.php');
 
+if(!isset($_GET['id']) && !isset($_GET['alias']) && cookie_group(NULL) != 1) {
+	$group = Table::Fetch('category', 1, 'id');
+	cookie_group($group);
+}
+
 if(!$INI['db']['host']) Utility::Redirect( WEB_ROOT . '/install.php' );
 
 $request_uri = 'index';
@@ -12,7 +17,7 @@ if (!isset($_COOKIE['popup']))
 	$sPopup = 1;
 	setcookie('popup', 1, 	time() + 365 * 86400, '/');
 }
-
+// print_r($_COOKIE);
 if ($team) {
 	$_GET['id'] = abs(intval($team['id']));
 	die(require_once('view.php'));
